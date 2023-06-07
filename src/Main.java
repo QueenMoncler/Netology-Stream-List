2c35da76eac6d711cbcfe999fe77ab3e0984d433
queen <Queenmoncler@gmail.com>
2023-06-07 21:41:58 +0300

    Add new Branch

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        Collections.sort(intList, Collections.reverseOrder());
 
-        ArrayList<Integer> answerList = new ArrayList<>();
-        for(int v:intList) {
-            if (v > 0) {
-                if (v % 2 == 0) {
-                    answerList.add(v);
-                }
-            } else {
-                break;
-            }
-        }
-
-        Collections.reverse(answerList);
-        System.out.println("Решено без StreamAPI " + answerList);
-
-        StreamMain.streamMain();
 
     }
 }
