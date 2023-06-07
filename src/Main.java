c36a5853e2dd0911c87a947322510456110e1b06
queen <Queenmoncler@gmail.com>
2023-06-07 21:38:15 +0300

    Problem Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList, Collections.reverseOrder());
+
+        ArrayList<Integer> answerList = new ArrayList<>();
+        for(int v:intList) {
+            if (v > 0) {
+                if (v % 2 == 0) {
+                    answerList.add(v);
+                }
+            } else {
+                break;
+            }
+        }
+
+        Collections.reverse(answerList);
+        System.out.println("Решено без StreamAPI " + answerList);
+
+        StreamMain.streamMain();
+
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamMain {
+    public static void streamMain(){
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> streamAnswerList = new ArrayList<>();
+
+        Stream<Integer> stream = intList.stream();
+        stream.filter(x -> x>0).filter(x -> x % 2 == 0).sorted(Comparator.naturalOrder()).forEach(w->streamAnswerList.add(w));
+
+        System.out.println("Сортировка c использованием StreamAPI " + streamAnswerList);
+    }
 }
