0428cccde70a03704cc8cfc92f41d44b7a987a0c
queen <Queenmoncler@gmail.com>
2023-06-08 21:55:39 +0300

    Problem Solution

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import PersonInfo.Person;
 import PersonInfo.Sex;
 
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +11,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 100; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -21,6 +21,28 @@ public class Main {
             );
         }
 
+
+        System.out.println("Количество несовершеннолетних: " +
+                "\n" + persons.stream().filter(x -> x.getAge() < 18).count());
+
+
+        System.out.println("\nCписок фамилий призывников: " +
+                "\n" + persons.stream()
+                .filter(x -> x.getAge() >= 18 & x.getAge() <= 27)
+                .map(e -> e.getFamily())
+                .collect(Collectors.toList()));
+
+
+        System.out.println("\nCписок потенциально работоспособных людей с высшим образованием: " +
+                "\n" + persons.stream().filter(x -> {
+                    if (x.getSex() == Sex.WOMAN & x.getAge() >= 18 & x.getAge() <= 60) return true;
+                    else if (x.getSex() == Sex.MAN & x.getAge() >= 18 & x.getAge() <= 65) return true;
+                    return false;
+                })
+                .filter(w -> w.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(e -> e.getFamily()))
+                .map(e -> e.getFamily())
+                .collect(Collectors.toList()));
 
 
     }
